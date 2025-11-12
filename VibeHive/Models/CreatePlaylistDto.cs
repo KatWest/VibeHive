998ad7e1d378fbb5e6a91c762f8ad23f48d14db5
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2025-11-11 21:53:45 -0800

    added models for form

---
+++ VibeHive/Models/CreatePlaylistDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VibeHive.Models
+{
+    public class CreatePlaylistDto
+    {
+        public string Name { get; set; }
+        public bool isCollaborative { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
